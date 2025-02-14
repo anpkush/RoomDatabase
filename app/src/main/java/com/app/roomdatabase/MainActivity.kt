cd1d67b22293770c4dfc4129a30b8038f2250716
Anupriyanshu kushwaha <anp.flybizz@gmail.com>
2025-02-14 17:49:25 +0530

    First Commit

---
+++ app/src/main/java/com/app/roomdatabase/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.app.roomdatabase
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    lateinit var databse : ContactDatabse
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        databse = Room.databaseBuilder(applicationContext,
+            ContactDatabse::class.java,
+            "contactDB").build()
+
+        GlobalScope.launch(){
+            databse.contactDao().insertContact(Contact(0,"Anup","9838160000"))
+        }
+    }
+}
